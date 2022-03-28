f79660b3ef1d815908e55336ebf24d5021d8cb57
MRTater <925955711@qq.com>
2022-03-28 17:58:42 +0800

    update

--- ImageNet/Baseline_train.sh
+++ ImageNet/Baseline_train.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ImageNetBKDLyy
 #SBATCH --mail-user=yyluo9@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/BKD.log
+#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/Baseline.log
 #SBATCH --gres=gpu:4
 #SBATCH -c 40
 #SBATCH --constraint=ubuntu18,highcpucount
