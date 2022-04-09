47852eba0d9b9a730762da6c728db3cda46593be
MRTater <925955711@qq.com>
2022-04-09 10:36:17 +0800

    Update Resnet56_Resnet20_imb.sh

--- CIFAR-100/Resnet56_Resnet20_imb.sh
+++ CIFAR-100/Resnet56_Resnet20_imb.sh
@@ -9,6 +9,6 @@
 #SBATCH -p batch_72h
 
 source activate KD
-python3 train.py --model resnet20 --teacher resnet56 \
+python3 train_imb.py --model resnet20 --teacher resnet56 \
  --teacher-weight checkpoints/CIFAR100V2_resnet56__baseline1_best.pt \
  --kd-loss-weight 0.6 --suffix reviewkd1 --dataset CIFAR100V2 --imb_factor 0.01
