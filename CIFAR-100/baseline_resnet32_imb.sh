124e0626edcdc19f37d3cd9a6d57fcc6476736f9
MRTater <925955711@qq.com>
2022-04-10 20:04:42 +0800

    update

--- CIFAR-100/baseline_resnet32_imb.sh
+++ CIFAR-100/baseline_resnet32_imb.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ImageNetBKDLyy
 #SBATCH --mail-user=yyluo9@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100/R32.log
+#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb/R32.log
 #SBATCH --gres=gpu:4
 #SBATCH -c 40
 #SBATCH --constraint=ubuntu18,highcpucount
