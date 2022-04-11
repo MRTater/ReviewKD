fd982fa7edb8fb3386a6379a2e246b5c33f80710
MRTater <925955711@qq.com>
2022-04-11 17:10:50 +0800

    script

--- CIFAR-100/Resnet56_Resnet56_imb_256.sh
+++ CIFAR-100/Resnet56_Resnet56_imb_256.sh
@@ -2,13 +2,13 @@
 #SBATCH --job-name=ImageNetBKDLyy
 #SBATCH --mail-user=yyluo9@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb/R56_R56.log
+#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb_256/R56_R56.log
 #SBATCH --gres=gpu:4
 #SBATCH -c 40
 #SBATCH --constraint=ubuntu18,highcpucount
 #SBATCH -p batch_72h
 
 source activate KD
-python3 ../train_imb.py --model resnet56 --teacher resnet56 \
+python3 train_imb.py --model resnet56 --teacher resnet56 \
  --teacher-weight checkpoints/CIFAR100V2_resnet56__baseline1_best_256.pt \
  --kd-loss-weight 0.6 --suffix reviewkd1 --dataset CIFAR100V2 --imb_factor 0.01 --batch_size 256
--- CIFAR-100/script_256/Resnet110_Resnet110_imb_256.sh
+++ CIFAR-100/script_256/Resnet110_Resnet110_imb_256.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=ImageNetBKDLyy
 #SBATCH --mail-user=yyluo9@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb/R110_R110.log
+#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb_256/R110_R110.log
 #SBATCH --gres=gpu:4
 #SBATCH -c 40
 #SBATCH --constraint=ubuntu18,highcpucount
