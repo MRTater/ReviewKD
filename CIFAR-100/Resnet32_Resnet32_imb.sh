00187a26f3b4f9802bc9514526da535e882c832f
MRTater <925955711@qq.com>
2022-04-15 00:51:06 +0800

    Update Resnet32_Resnet32_imb.sh

--- CIFAR-100/Resnet32_Resnet32_imb.sh
+++ CIFAR-100/Resnet32_Resnet32_imb.sh
@@ -2,11 +2,11 @@
 #SBATCH --job-name=ImageNetBKDLyy
 #SBATCH --mail-user=yyluo9@cse.cuhk.edu.hk
 #SBATCH --mail-type=ALL
-#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb/R110_R32.log
+#SBATCH --output=/research/dept8/fyp21/lj2104/lyy/ReviewKD/log/CIFAR100_imb/R32_R32.log
 #SBATCH --gres=gpu:4
 #SBATCH -c 40
 #SBATCH --constraint=ubuntu18,highcpucount
-#SBATCH -p batch_72h
+#SBATCH -p gpu_24
 
 source activate KD
 python3 train_imb.py --model resnet32 --teacher resnet32 \
