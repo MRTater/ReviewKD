4d1d9e0db598f38b942630b506c9b07f272de586
MRTater <925955711@qq.com>
2022-04-14 23:58:07 +0800

    Update MyEnsemble.py

--- CIFAR-100/model/MyEnsemble.py
+++ CIFAR-100/model/MyEnsemble.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+from .resnet_cifar import build_resnet_backbone
+
 
 class MyEnsemble(nn.Module):
 
@@ -8,14 +10,17 @@ class MyEnsemble(nn.Module):
         super(MyEnsemble, self).__init__()
 
         with open(modelA, 'rb') as f:
-            self.modelA.load_state_dict(torch.load(f))
-            # modelA.eval()
+            self.modelA = build_resnet_backbone(depth=int(32), num_classes=100)
+            modelA.load_state_dict(torch.load(f))
+            modelA.eval()
         with open(modelB, 'rb') as f:
-            self.modelB.load_state_dict(torch.load(f))
-            # modelB.eval()
+            self.modelB = build_resnet_backbone(depth=int(32), num_classes=100)
+            modelB.load_state_dict(torch.load(f))
+            modelB.eval()
         with open(modelC, 'rb') as f:
-            self.modelC.load_state_dict(torch.load(f))
-            # modelC.eval()
+            self.modelC = build_resnet_backbone(depth=int(32), num_classes=100)
+            modelC.load_state_dict(torch.load(f))
+            modelC.eval()
 
     def forward(self, x):
         out1 = self.modelA(x)
